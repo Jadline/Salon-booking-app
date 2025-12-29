9ec6b4f8aa788cca42c33cb9c6542f7e89e1f444
Jadline <njerijadline@gmail.com>
2025-12-29 19:30:12 +0300

    enabling cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,44 @@
-const express = require('express')
-const morgan = require('morgan')
-const clientRouter = require('./Routes/clientsRouter')
-const cors = require('cors')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const clientRouter = require('./Routes/clientsRouter');
 
-const app = express()
-
-app.use(cors())
-
-app.use(morgan('dev'))
-
-app.use(express.json())
+const app = express();
 
 
-app.use('/api/v1/clients',clientRouter)
+app.use(cors({
+  origin: 'https://salon-booking-app-vert.vercel.app/' 
+}));
+
+
+app.use(morgan('dev'));
+
+
+app.use(express.json());
+
+
+app.use('/api/v1/clients', clientRouter);
+
 
 app.get('/test', (req, res) => {
   res.send('Server is working');
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannot find ${req.originalUrl} on this server`
+  });
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong on the server'
+  });
 });
 
 module.exports = app;
