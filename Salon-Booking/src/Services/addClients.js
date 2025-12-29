5b397f4c50d09d86abec700f0a713213b6a51971
Jadline <njerijadline@gmail.com>
2025-12-29 19:11:20 +0300

    updated api route

--- Salon-Booking/src/Services/addClients.js
+++ Salon-Booking/src/Services/addClients.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 // const CLIENTS_URL = 'http://127.0.0.1:3000/api/v1/clients'
 export async function createClient(clientData) {
   try {
-    const response = await axios.post(BASE_URL, clientData);
+    const response = await axios.post(`${BASE_URL}/api/v1/clients`, clientData);
     return response.data.data.client;
   } catch (err) {
     console.error('Error creating client', err);
--- Salon-Booking/src/Services/getClients.js
+++ Salon-Booking/src/Services/getClients.js
@@ -3,7 +3,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 // const CLIENTS_URL = 'http://127.0.0.1:3000/api/v1/clients'
 export async function fetchClients(){
     try {
-          const response  = await axios.get(BASE_URL)
+          const response  = await axios.get(`${BASE_URL}/api/v1/clients`)
           console.log(response.data.data.clients)
           return response.data.data.clients
     }
